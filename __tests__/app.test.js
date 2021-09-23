446a2070bf9e1d657fc327e516d06f2b2a05ce8a
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-09-23 16:19:46 -0700

    failing api get test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,17 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import fetchJokeAPI from '../lib/utils/jokeAPI.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('gets a random safe joke from get /', async() => {
+    return await request(app).get(fetchJokeAPI()).then(res => {
+      expect(res.body).toEqual(null);
+    });
   });
 
   afterAll(() => {
