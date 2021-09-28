1e6749ca7e3635e61e69420d61d1d5f38ea8f806
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-09-28 16:34:39 -0700

    failing patch test for puns

--- __tests__/pun.test.js
+++ __tests__/pun.test.js
@@ -35,48 +35,45 @@ describe('demo routes', () => {
   it('reads all jokes in /api/punnyjoke', async() => {
     await request(app).post('/api/punnyjokes')
       .send({
-        category: 'Programming',
+        category: 'Pun',
         typeOf: 'twopart',
-        setup: 'A web developer walks into a restaurant.',
-        delivery: 'He immediately leaves in disgust as the restaurant was laid out in tables.' });
+        setup: 'What do you call a witch at the beach?',
+        delivery: 'A Sandwich.' });
     return await request(app)
       .get('/api/punnyjokes/all').then(res => {
         expect(res.body).toEqual([{ 
           id: '1',
-          category: 'Programming',
+          category: 'Pun',
           typeOf: 'twopart',
-          setup: 'A web developer walks into a restaurant.',
-          delivery: 'He immediately leaves in disgust as the restaurant was laid out in tables.' }]);
+          setup: 'What do you call a witch at the beach?',
+          delivery: 'A Sandwich.' }]);
       });
   });
 
-  //   it('updates a joke by id', async() => {
-  //     await request(app).post('/api/doublejokes')
-  //       .send({
-  //         category: 'Programming',
-  //         typeOf: 'twopart',
-  //         setup: 'A web developer walks into a restaurant.',
-  //         delivery: 'He immediately leaves in disgust as the restaurant was laid out in tables.' });
-  //     return await request(app).patch('/api/doublejokes/1')
-  //       .send({
-  //         id:'1',
-  //         category: 'Programming',
-  //         typeOf: 'twopart', 
-  //         setup: 'A web developer team walks into a restaurant.',
-  //         delivery: 'They immediately leave in anger as the restaurant could not join tables.' 
-  //       }).then(res => {
-  //         expect(res.body).toEqual(
-  //           { id: '1',
-  //             category: 'Programming',
-  //             typeOf: 'twopart',
-  //             setup: 'A web developer team walks into a restaurant.',
-  //             delivery: 'They immediately leave in anger as the restaurant could not join tables.' });
-  //       });
-  //   });
+  it('updates a joke by id', async() => {
+    await request(app).post('/api/punnyjokes')
+      .send({
+        category: 'Programming',
+        typeOf: 'twopart',
+        setup: 'A web developer walks into a restaurant.',
+        delivery: 'He immediately leaves in disgust as the restaurant was laid out in tables.' });
+    return await request(app).patch('/api/punnyjokes/1')
+      .send({
+        id:'1',
+        category: 'Programming',
+        typeOf: 'twopart', 
+        setup: 'A web developer team walks into a restaurant.',
+        delivery: 'They immediately leave in anger as the restaurant could not join tables.' 
+      }).then(res => {
+        expect(res.body).toEqual(
+          { id: '1',
+            category: 'Programming',
+            typeOf: 'twopart',
+            setup: 'A web developer team walks into a restaurant.',
+            delivery: 'They immediately leave in anger as the restaurant could not join tables.' });
+      });
+  });
       
-      
-  
-    
 //   it('should delete a joke', async () => {
 //     await request(app).post('/api/doublejokes')
 //       .send({
