b4b3bbbba3f497a6380c359d05145861ac6ddc35
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-09-27 17:01:43 -0700

    passing get all from local doublejokes - added /all to get route

--- __tests__/double.test.js
+++ __tests__/double.test.js
@@ -40,7 +40,7 @@ describe('demo routes', () => {
         setup: 'A web developer walks into a restaurant.',
         delivery: 'He immediately leaves in disgust as the restaurant was laid out in tables.' });
     return await request(app)
-      .get('/api/doublejokes/').then(res => {
+      .get('/api/doublejokes/all').then(res => {
         expect(res.body).toEqual([{ id: '1',
           category: 'Programming',
           typeOf: 'twopart',
