b68e1200b110b05e3bcfbc9ea564faa4bf22ded7
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-10-05 21:52:52 -0700

    failing delete prog joke

--- __tests__/progJokes.test.js
+++ __tests__/progJokes.test.js
@@ -84,22 +84,20 @@ describe('demo routes', () => {
       });
   });
       
-//   it('should delete a joke', async () => {
-//     await request(app).post('/api/programmingjokes')
-//       .send({
-//         category: 'Pun',
-//         typeOf: 'twopart',
-//         setup: 'Did you hear about the cheese factory that exploded in France?',
-//         delivery: 'There was nothing but de brie.' });
-//     return request(app).delete('/api/programmingjokes/1').then(res => {
-//       expect(res.body).toEqual({
-//         id: '1',
-//         category: 'Pun',
-//         typeOf: 'twopart',
-//         setup: 'Did you hear about the cheese factory that exploded in France?',
-//         delivery: 'There was nothing but de brie.' });
-//     });
-//   });
+  it('should delete a joke', async () => {
+    await request(app).post('/api/programmingjokes')
+      .send({
+        category: 'Programming',
+        typeOf: 'single',
+        joke: '"Honey, go to the store and buy some eggs."\n"OK."\n"Oh and while you\'re there, get some milk."\n"He never returned."' });
+    return request(app).delete('/api/programmingjokes/1').then(res => {
+      expect(res.body).toEqual({
+        id: '1',
+        category: 'Programming',
+        typeOf: 'single',
+        joke: '"Honey, go to the store and buy some eggs."\n"OK."\n"Oh and while you\'re there, get some milk."\n"He never returned."' });
+    });
+  });
 });
 
 
